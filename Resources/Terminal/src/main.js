01450d3a7a6c311d4da821e0a0e8875be7222ac6
alifhughes <alistair.hughes@students.plymouth.ac.uk>
2017-08-13 15:36:26 +0100

    setting up file

--- Resources/Terminal/src/main.js
+++ Resources/Terminal/src/main.js
@@ -1,7 +1,9 @@
+#!/usr/bin/env node
+
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-//var xhr = new XMLHttpRequest();
+var xhr = new XMLHttpRequest();
 
 // print process.argv
-process.argv.forEach(function (val, index, array) {
-  console.log(index + ': ' + val);
+process.argv.slice(2).forEach(function (val, index, array) {
+      console.log(index + ': ' + val);
 });
