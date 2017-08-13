367106a43562621b9c3ec0063feefe4150f3d303
alifhughes <alistair.hughes@students.plymouth.ac.uk>
2017-08-13 17:52:50 +0100

    bare bones no real functionality yet

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
 # vinyl-recommender
 
-## Primary objectives
+### Primary objectives
 
 1. To retrieve a single viynl recommendation is not always the same.
+    - Copy the uri link to clipboard or wait till listen command is entered and opens a browser with it in
 2. To retrieve a single vinyl recommendation based off of a specified genre.
     - Accept genre input via command line arguments
 3. Ouput the retrieved result in manner in which allows easy access to the vinyl page information.
 4. Allow for the vinyl to be bought via commands.
+5. To retrieve random release from an artist.
+
+
+### How to use
+
+##### Random release
+Brings up release
+
+No arguments
+`./main.js`
+
+type `listen` to open new browser with one of the release songs
+
+type `open` to open the release page in the browser
+
+type `next` to get a different release
--- Resources/Terminal/src/main.js
+++ Resources/Terminal/src/main.js
@@ -1,9 +1,77 @@
 #!/usr/bin/env node
 
+// Init xhr
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
+var open = require("open");
 
-// print process.argv
-process.argv.slice(2).forEach(function (val, index, array) {
-      console.log(index + ': ' + val);
+// Create discogs client
+var Discogs = require('disconnect').Client;
+var db = new Discogs().database();
+
+// Set the limit for the id
+var releaseIdLimit = 11000000;
+
+// Get arguments array
+var args = process.argv.slice(2);
+
+db.getRelease(9815160, function(err, data){
+    console.log(data);
 });
+
+// Call the argument engine
+argumentEngine();
+
+/**
+ * The argument engine, decides how to act depeding on arguments
+ */
+function argumentEngine () {
+
+    // Check if any arguments at all
+    if (!args.length) {
+        // No arguments, get a random release or artist
+        open('https://www.google.co.uk/');
+        // Rand 0 or 1
+        // Rand number for id
+        //      if call is 404
+        //      get diff rand num
+        //      continue
+
+        return;
+    }
+
+    // Switch on the first argument flag
+    switch (args[0]) {
+
+        case '-h' :
+            // Show help
+            help();
+            break;
+
+        case '-g' :
+            break;
+
+        case '-a' :
+            break;
+
+        default :
+            // Fall through
+
+            console.log(
+                    'No correct parameter entered,' +
+                    ' try entering \'-h\' command to see the help.'
+                );
+
+    }
+
+};
+
+/**
+ * Logs out the help that contains options and how to use program
+ */
+function help () {
+    console.log('HELP!');
+};
+
+function prettifyJson () {
+};
